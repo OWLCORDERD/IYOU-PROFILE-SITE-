9693aac3c76cf865f7e7cd7376933237bdd8f337
MIYEOK <lim5570@naver.com>
2022-02-02 20:30:55 +0900

    02/02 commit

---
+++ SliderText.js
@@ -0,0 +1,12 @@
+let slideTexts = document.querySelector('.PART_content');
+
+window.addEventListener("scroll", function(){
+    let value = window.scrollY;
+    console.log("ScrollY", value);
+
+    if(value > 1200){
+        slideTexts.style.animation = "disappear 1s ease-out forwards";
+    }else{
+        slideTexts.style.animation = "slideText 1s ease-in";
+    }
+});
