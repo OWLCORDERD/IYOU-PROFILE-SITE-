08ab810e03fe45676668532365076cec3398573e
MIYEOK <lim5570@naver.com>
2022-02-05 23:54:41 +0900

    2/06 commit

--- active.js
+++ active.js
@@ -1,6 +1,8 @@
-let btn = document.querySelector('.close-btn');
-let sidebar = document.querySelector('aside');
+let btn = document.querySelector(".close-btn");
+let sidebar = document.querySelector("aside");
+let section = document.querySelector("section");
 
-btn.onclick = function(){
-    sidebar.classList.toggle("active");
-}
+btn.onclick = function () {
+  sidebar.classList.toggle("active");
+  section.classList.toggle("active");
+};
