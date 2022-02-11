aae38d09ebb9f49a8adb47ec48f94633870693be
MIYEOK <lim5570@naver.com>
2022-02-12 01:18:01 +0900

    2/12 commit

--- SliderText.js
+++ SliderText.js
@@ -2,7 +2,6 @@ let slideTexts = document.querySelector(".PART_content");
 
 window.addEventListener("scroll", function () {
   let value = window.scrollY;
-  console.log("ScrollY", value);
 
   if (value > 1300) {
     slideTexts.style.animation = "disappear 1s ease-out forwards";
